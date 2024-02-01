d675f918a397c5f6113bfd959b60c5f22ce0ce44
deandrebrown01 <dbrown@brooklynsteamcenter.org>
2024-02-01 10:49:18 -0500

    almost finished gotta work on it more

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def Index():
     return render_template("home.html.jinja")
+
+@app.route("/register", methods =["POST", "GET"])
+def register():
+    return render_template("register.html.jinja")
+
